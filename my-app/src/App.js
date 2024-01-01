b3fbf93e803099149023bc6dbff659bc427fc6c6
ChaeYi <chaeyi990731@gmail.com>
2024-01-01 23:16:48 +0900

    함수 주석처리

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,14 @@
+// React 라이브러리와 그 안에 있는 useEffect와 useState라는 두 가지 훅을 가져온다.
+// React 훅(Hook)은 React의 기능을 함수형 컴포넌트에서 사용할 수 있게 해주는 기능
+//!(후에 노션에 정리할것)
+
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import info from './info.json';
 
 const App = () => {
   const [people, setPeople] = useState([]);
+  // 이 코드는 people이라는 상태와 그 상태를 변경하는 함수인 setPeople을 생성합니다. 초기 상태는 빈 배열
   const [selectedPerson, setSelectedPerson] = useState(null);
   const [searchName, setSearchName] = useState('');
   const [selectedName, setSelectedName] = useState('');
@@ -14,6 +19,7 @@ const App = () => {
   }, []);
 
   const onClickName = (name) => {
+    //이 코드는 이름을 클릭했을 때 실행되는 함수를 정의합니다. 이 함수는 선택된 이름을 selectedName 상태에 저장하고, 해당 이름의 사람 정보를 selectedPerson 상태에 저장
     setSelectedName(name);
     const person = people.find((p) => p.name === name);
     setSelectedPerson(person);
