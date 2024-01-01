bc0b0fae52d78b12189744f11acce176822baeac
ChaeYi <chaeyi990731@gmail.com>
2024-01-01 19:52:48 +0900

    static으로 작업한다면 따로 css,js파일을 서빙 할 필요가 없다

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const port = 3000;
 
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, './html/index.html');
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      res.status(500).send('Internal Server Error');
-    }
-  });
-});
+app.use(express.static('html'));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
